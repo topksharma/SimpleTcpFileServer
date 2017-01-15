27a8b74f883049e88d67c7a14a3db5f1adbf3e62
topksharma <topksharma@gmail.com>
2017-01-15 11:40:44 +0100

    Committed common files

---
+++ Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRepo.Tcp.Common
+{
+	public static class Utils
+	{
+		public static string RemoveInvalidChars(this string fileName)
+		{
+			return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(), string.Empty));
+		}
+		public static void GetDirectorySize(DirectoryInfo dInfo, ref int numOfFiles, ref long size)
+		{
+			try
+			{
+				if (dInfo.Exists)
+				{
+					//DirectoryInfo dInfo = new DirectoryInfo(dirName);
+					FileInfo[] files = dInfo.GetFiles();
+					numOfFiles += files.Length;
+					for (int i = 0; i < files.Length; i++)
+					{
+						size += files[i].Length;
+					}
+
+					foreach (DirectoryInfo info in dInfo.GetDirectories())
+					{
+						GetDirectorySize(info, ref numOfFiles, ref size);
+					}
+				}
+			}
+			catch (Exception)
+			{
+			}
+		}
+	}
+}
